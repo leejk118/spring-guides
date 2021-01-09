7873e4352c68b779feaa37108043ad868eebb53e
leejk118 <leejk118@gmail.com>
2021-01-10 08:40:52 +0900

    init

---
+++ src/test/java/com/jklee/demo/JkleeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jklee.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JkleeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
